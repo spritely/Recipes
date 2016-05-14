6f0db0515c71fc2d9e32551d28b2173b69a3962e
Victor Robinson <victorandtina@hotmail.com>
2016-05-14 14:12:36 -0400

    Add Base64UrlExtensions and JsonWebToken recipes, change nuget package install target locations, update copyright statements, and reduce internal dependencies on Functional

--- Recipes/ProgressReporter/ProcessingStatus.cs.pp
+++ Recipes/ProgressReporter/ProcessingStatus.cs.pp
@@ -8,7 +8,7 @@
 // </auto-generated>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Spritely.Recipes
+namespace $rootnamespace$
 {
     using System;
 
--- Recipes/ProgressReporter/ProgressReporter.cs.pp
+++ Recipes/ProgressReporter/ProgressReporter.cs.pp
@@ -8,7 +8,7 @@
 // </auto-generated>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Spritely.Recipes
+namespace $rootnamespace$
 {
     using System;
     using System.Collections.Generic;
