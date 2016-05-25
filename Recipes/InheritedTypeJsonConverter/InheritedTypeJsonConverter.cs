f8b230e9977e9d8ae107e404397b1036f821745d
Suraj Gupta <surajg@gmail.com>
2016-05-25 12:01:26 -0400

    InhertedTypeJsonConverter now supports types with constructors Also, in cases where there is ambiguity about which child type is properly represented by the JSON, the old methodology would pick a child-type instead of throwing

--- Recipes/InheritedTypeJsonConverter/InheritedTypeJsonConverter.cs
+++ Recipes/InheritedTypeJsonConverter/InheritedTypeJsonConverter.cs
@@ -15,7 +15,6 @@ namespace Spritely.Recipes
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.Globalization;
-    using System.IO;
     using System.Linq;
     using System.Reflection;
 
@@ -54,34 +53,98 @@ namespace Spritely.Recipes
         }
 
         /// <inheritdoc />
+        /// <remarks>
+        /// - Methodology
+        ///   - Get all child types of the specified type.
+        ///   - Filter to child types where every 1st level JSON property is either a public (accessor is public)
+        ///     property or a public field of the child type, using case-insensitive matching.  Child types passing
+        ///     this filter are called 'candidates'.
+        ///   - If there are no candidates, then throw.
+        ///   - For all candidates, ask the serializer to deserialize the JSON as the candidate type.  Catch and 
+        ///     ignore exceptions when attempting to deserialize.
+        ///   - If more than one candidate successfully deserializes, then throw.
+        /// - Using the serializer to deserialize enables the method to support types with constructors,
+        ///   which JSON.net does well out-of-the-box and which would be consumbersome to emulate.
+        /// - This method does not consider > 1st level JSON properties to determine candidates.  In other words,
+        ///   it is not matching on the fields/properties of the child's fields/properties (e.g. the match is done 
+        ///   on Dog.Owner, not Dog.Owner.OwnersAddress).  The issue is that that kind of matching would require
+        ///   complex logic.  For example, Strings have properties such as Length which would need to be ignored 
+        ///   when reflecting.  Similarly, all fields/properites of value-types would need to be ignored.  There 
+        ///   are likely other corner-cases.  To keep things simple, if the 1st level properties match the type's
+        ///   properties and fields, by name, we hand-off the problem to the serializer and let it throw if
+        ///   there is some incompatability deep in the field/property hierarchy.
+        /// - This method does not consider the type of objects containted within JSON arrays to determine
+        ///   candidates.  This is difficult because JSON arrays can contain a mix of types (just likes .net 
+        ///   objects) and every element would have to be deserialized and matched against the child's IEnumerable
+        ///   type and undoubtedly complexity would arrise from dealing with generics and the vast implementations
+        ///   of IEnumerable.  Like the bullet above, we simply hand-off the problem to the serializer.
+        /// - If properties or fields are removed from a child type after it has been serialized, then the JSON
+        ///   will not deserialize properly because that child type will no longer be a candidate.  If, however,
+        ///   properties or fields are added to the child type, then the child type will continue to be a
+        ///   candidate for the serialized JSON.
+        /// - If the user serializes private or internal fields/properties, then this method will not work because
+        ///   it only looks for public fields/properties.  We cannot bank on the JSON having been serialized by
+        ///   the same serialized passed to this method.  Even if we could, the serializer is so highly 
+        ///   configurable that it would be difficult to determine whether or which internal or private fields
+        ///   or properites are serialized.
+        /// - It's OK if the JSON is serialized with NullValueHandling.Ignore because the candidate filter tries
+        ///   to find all JSON properties in child type's properties/fields, and not vice-versa.  However, depending
+        ///   on how permissive the serializer's Contract Resolver is, those candidates may or may not be able to
+        ///   be deserialized.  For example, if constructor parameters are required and a particular parameter is
+        ///   excluded from the JSON, then that type cannot be deserialized.
+        /// </remarks>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var jsonObject = JObject.Load(reader);
-            var sourceKeys = GetKeys(jsonObject).Select(k => k.Key).ToList();
+            var childTypes = GetChildTypes(objectType).ToList();
 
-            var childTypes = GetChildTypes(objectType);
+            var jsonProperties = GetProperties(jsonObject);
+            var candidateChildTypes = new List<Type>();
+            foreach (var childType in childTypes)
+            {
+                var childTypeProperties = childType.GetProperties().Select(t => t.Name).ToList();
+                var childTypeFields = childType.GetFields().Select(t => t.Name).ToList();
+                var childTypePropertiesAndFields = new HashSet<string>(
+                    childTypeProperties.Concat(childTypeFields),
+                    StringComparer.OrdinalIgnoreCase);
+                if (jsonProperties.All(p => childTypePropertiesAndFields.Contains(p)))
+                {
+                    candidateChildTypes.Add(childType);
+                }
+            }
 
-            var testObjects = childTypes.Select(Activator.CreateInstance)
-                .Select(instance => new { Instance = instance, SerializedInstance = Serialize(serializer, instance) })
-                .ToList();
+            var deserizliedObjects = new List<object>();
+            foreach (var candidateChildType in candidateChildTypes)
+            {
+                try
+                {
+                    var deserializedObject = serializer.Deserialize(jsonObject.CreateReader(), candidateChildType);
+                    if (deserializedObject != null)
+                    {
+                        deserizliedObjects.Add(deserializedObject);
+                    }
+                }
+                catch (JsonException)
+                {
+                }
+            }
 
-            var testKeys = testObjects.ToDictionary(
-                to => to,
-                to => GetKeys(to.SerializedInstance).Select(kvp => kvp.Key).ToList());
-
-            var target = testObjects.Select(to => new { TestObject = to, Keys = testKeys[to] })
-                .OrderBy(t => t.Keys.Count)
-                .FirstOrDefault(t => !sourceKeys.Except(t.Keys).Any()); // Any keys in source that are not in the target object?
-
-            if (target == null)
+            if (deserizliedObjects.Count == 0)
             {
                 throw new JsonSerializationException(
                     string.Format(CultureInfo.InvariantCulture, "Unable to deserialize to type {0}, value: {1}", objectType, jsonObject));
             }
 
-            serializer.Populate(jsonObject.CreateReader(), target.TestObject.Instance);
+            if (deserizliedObjects.Count > 1)
+            {
+                var matchingTypes =
+                    deserizliedObjects.Select(_ => _.GetType().Name)
+                        .Aggregate((running, current) => running + " | " + current);
+                throw new JsonSerializationException(
+                    string.Format(CultureInfo.InvariantCulture, "The json string can be deserialized into multiple types: {0}, value: {1}", matchingTypes, jsonObject));
+            }
 
-            return target.TestObject.Instance;
+            return deserizliedObjects.Single();
         }
 
         /// <inheritdoc />
@@ -91,65 +154,16 @@ namespace Spritely.Recipes
             jsonObject.WriteTo(writer);
         }
 
-        private static JObject Serialize(JsonSerializer serializer, object instance)
+        private static List<string> GetProperties(JToken node)
         {
-            // Copy everything except NullValueHandling (need to make sure all keys are included in
-            // serialized json)
-            var jsonSerializer = new JsonSerializer
-            {
-                Binder = serializer.Binder,
-                CheckAdditionalContent = serializer.CheckAdditionalContent,
-                ConstructorHandling = serializer.ConstructorHandling,
-                Context = serializer.Context,
-                ContractResolver = serializer.ContractResolver,
-                Culture = serializer.Culture,
-                DateFormatHandling = serializer.DateFormatHandling,
-                DateFormatString = serializer.DateFormatString,
-                DateParseHandling = serializer.DateParseHandling,
-                DateTimeZoneHandling = serializer.DateTimeZoneHandling,
-                DefaultValueHandling = serializer.DefaultValueHandling,
-                EqualityComparer = serializer.EqualityComparer,
-                FloatFormatHandling = serializer.FloatFormatHandling,
-                FloatParseHandling = serializer.FloatParseHandling,
-                Formatting = serializer.Formatting,
-                MaxDepth = serializer.MaxDepth,
-                MetadataPropertyHandling = serializer.MetadataPropertyHandling,
-                MissingMemberHandling = serializer.MissingMemberHandling,
-                NullValueHandling = NullValueHandling.Include,
-                ObjectCreationHandling = serializer.ObjectCreationHandling,
-                PreserveReferencesHandling = serializer.PreserveReferencesHandling,
-                ReferenceLoopHandling = serializer.ReferenceLoopHandling,
-                ReferenceResolver = serializer.ReferenceResolver,
-                StringEscapeHandling = serializer.StringEscapeHandling,
-                TraceWriter = serializer.TraceWriter,
-                TypeNameAssemblyFormat = serializer.TypeNameAssemblyFormat,
-                TypeNameHandling = serializer.TypeNameHandling
-            };
+            var result = new List<string>();
 
-            foreach (var converter in serializer.Converters)
+            if (node.Type == JTokenType.Object)
             {
-                jsonSerializer.Converters.Add(converter);
+                result.AddRange(node.Children<JProperty>().Select(child => child.Name));
             }
 
-            using (var writer = new StringWriter(CultureInfo.InvariantCulture))
-            {
-                jsonSerializer.Serialize(writer, instance);
-                var json = writer.ToString();
-                var parsed = JObject.Parse(json);
-                return parsed;
-            }
-        }
-
-        private static IEnumerable<KeyValuePair<string, JToken>> GetKeys(JObject jObject)
-        {
-            var list = new List<KeyValuePair<string, JToken>>();
-
-            foreach (var token in jObject)
-            {
-                list.Add(token);
-            }
-
-            return list;
+            return result;
         }
 
         private IEnumerable<Type> GetChildTypes(Type type)
@@ -172,8 +186,7 @@ namespace Spritely.Recipes
                         })
                     .Where(
                         t =>
-                            t != null && t.IsClass && !t.IsAbstract && !t.IsGenericTypeDefinition && t != type && type.IsAssignableFrom(t) &&
-                            t.GetConstructor(Type.EmptyTypes) != null)
+                            t != null && t.IsClass && !t.IsAbstract && !t.IsGenericTypeDefinition && t != type && type.IsAssignableFrom(t))
                     .ToList();
 
                 allChildTypes.AddOrUpdate(type, t => childTypes, (t, cts) => childTypes);
